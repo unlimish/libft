fb731914a013dfbb37e76232351da512d273cbd4
unlimish <mail@unlimish.xyz>
2021-04-07 18:31:54 +0900

    🔧 Update (ft_substr): split functions

--- ft_substr.c
+++ ft_substr.c
@@ -1,5 +1,14 @@
 #include "libft.h"
 
+static	void	skip_start_str(char const *s, unsigned int start, size_t len)
+{
+	while (start != 0 && len != 0)
+	{
+		s++;
+		start--;
+	}
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
@@ -7,16 +16,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (s == NULL)
 		return (NULL);
-	(len > ft_strlen(s + start)) ? (result = malloc(ft_strlen(s + start) + 1)) :
-		(result = malloc(len + 1));
+	if (len > ft_strlen(s + start))
+		result = malloc(ft_strlen(s + start) + 1);
+	else
+		result = malloc(len + 1);
 	if (result == NULL || start >= ft_strlen(s))
 		return (result);
 	ptr_result = result;
-	while (start != 0 && len != 0)
-	{
-		s++;
-		start--;
-	}
+	skip_start_str(s, start, len);
 	while (len != 0 && *s != '\0')
 	{
 		*result = *s + start;
