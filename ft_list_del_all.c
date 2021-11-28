1c9cd520e65dfbc403d0580a9888354969a1db48
unlimish <me@unlimi.sh>
2021-11-28 23:59:28 +0900

    🚑 Fix: memory leak in ft_list func

--- ft_list_del_all.c
+++ ft_list_del_all.c
@@ -5,7 +5,7 @@ void	ft_list_del_all(t_dlist *list)
 	while (list->next != NULL)
 	{
 		list = list->next;
-		free(list);
+		free(list->prev);
 	}
 	free(list);
 }
--- ft_list_put.c
+++ ft_list_put.c
@@ -2,10 +2,13 @@
 
 void	ft_list_put(t_dlist *list)
 {
+	char *str;
 	while (list != NULL)
 	{
-		ft_putstr_fd(ft_itoa(list->value), STDOUT_FILENO);
+		str = ft_itoa(list->value);
+		ft_putstr_fd(str, STDOUT_FILENO);
 		ft_putstr_fd("\n", STDOUT_FILENO);
+		free(str);
 		list = list->next;
 	}
 }
